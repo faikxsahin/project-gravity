cc02c81fb9ea08fd1997b198f0f252bcaeabb38d
faikxsahin <faikxsahin@outlook.com>
2023-07-03 23:29:26 +0300

    Modified oscillator script for moving objects

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -7,7 +7,8 @@ public class Oscillator : MonoBehaviour
 
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField][Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        const float tau = Mathf.PI * 2;
+        
+        float cycles = Time.time / period; // Continually growing over time
+        float rawSinWave = Mathf.Sin(cycles * tau); // Going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // Recalculated to go from 0 to 1
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
